45212a434c05110490dfd1667a0fd9af5239aabc
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-10-11 15:41:04 +0530

    category image bug resolved

--- lib/Tool/CategoryImage.php
+++ lib/Tool/CategoryImage.php
@@ -10,12 +10,20 @@ class Tool_CategoryImage extends \xepan\cms\View_Tool{
 	function init(){
 		parent::init();
 
+		$cat_m = $this->add('xepan\commerce\Model_Category');
+		$cat_m->loadBy('slug_url',$_GET['parent_category_code']);	
+		$parent_category_id = $cat_m->id;
+		$cat_m->unload();	
+		$category_code = explode('/', $_GET['category_code']);
+		$cat_m->loadBy('slug_url',$category_code[1]);	
+		$category_id = $cat_m->id;
+
 		$categoryimage_m = $this->add('xepan\custom\Model_CategoryImage');
 		
 		if($this->options['show_image_of'] === "parent_category"){
-			$categoryimage_m->tryLoadBy('category_id',$_GET['parent_category_id']);
+			$categoryimage_m->tryLoadBy('category_id',$parent_category_id);
 		}else{
-			$categoryimage_m->tryLoadBy('category_id',$_GET['xsnb_category_id']);
+			$categoryimage_m->tryLoadBy('category_id',$category_id);
 		}
 
 		if($categoryimage_m->loaded())
