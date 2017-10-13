659965607100928f4655b0b410fa9e9242186d52
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-10-13 11:57:04 +0530

    tools migrated

---
+++ lib/Tool/Filter.php
@@ -0,0 +1,275 @@
+<?php
+
+namespace xepan\custom;
+
+class Tool_Filter extends \xepan\cms\View_Tool{
+	public $options = [
+			"show_price_filter"=>true,
+			"min_price"=>0,
+			"max_price"=>10,
+			"left_label" => "min",
+			"right_label" => "max",
+			"filter_type" => "",
+			"custom_template"=>''
+	];
+	public $header_view;
+
+	function init(){
+		parent::init();
+
+		$parent_category_id = $this->app->stickyGET('parent_category_code');
+
+		$form_layout = 'view/tool/filter/formsection';
+
+		if($this->options['custom_template']){
+			$path = getcwd()."/websites/".$this->app->current_website_name."/www/view/tool/filter".$this->options['custom_template'].".html";
+			if(file_exists($path)){
+				$form_layout = 'view/tool/filter/'.$this->options['custom_template'];
+			}else{
+				$this->add('View_Error')->set('Custom template not found.');
+				return; 
+			}
+		}
+		
+		$this->app->stickyGET('category_code');
+
+		$previous_selected_filter = json_decode($this->app->recall('filter'),true)?:[];
+		
+		$model_filter = $this->add('xepan\commerce\Model_Filter');
+
+		$spec_array = [];
+		$avil_size_array = [];
+
+		$category_code = explode('/', $_GET['category_code']);
+		$category_m = $this->add('xepan\commerce\Model_Category');
+		$category_m->tryLoadBy('slug_url',$category_code[1]);
+
+		$parent_category_m = $this->add('xepan\commerce\Model_Category');
+		$parent_category_m->tryLoadBy('slug_url',$_GET['parent_category_code']);
+
+		if($xsnb_category_id = $category_m->id){			
+			$assoc_m = $this->add('xepan\commerce\Model_CategoryItemAssociation');
+			$assoc_m->addCondition('category_id',$xsnb_category_id);
+
+			$item_id = [];
+			foreach ($assoc_m as $assoc) {
+				$item_id [] = $assoc['item_id'];
+			}
+
+			foreach ($item_id as $item){				
+				$item_m = $this->add('xepan\commerce\Model_Item');	
+				$item_m->tryLoad($item);
+
+				if(!$item_m->loaded())
+					continue;
+
+				if($this->options['filter_type'] == 'Product'){
+					if($item_m['hide_in_product'])
+						continue;
+				}
+
+				if($this->options['filter_type'] == 'Shop'){
+					if($item_m['hide_in_shop'])
+						continue;
+
+					$item_stock_m = $this->add('xepan\commerce\Model_ItemStock');
+					$item_stock_m->addCondition('item_id',$item_m->id);
+
+					
+					if($item_stock_m->count()->getOne() <1)
+						continue;
+					else
+						foreach ($item_stock_m as $is) {																					
+							if($is['category'] == $parent_category_m->id)
+								$avil_size_array[] = $is['size'];
+						}
+				}
+
+				$spec_m = $this->add('xepan\commerce\Model_Item_Specification');
+				$spec_m->loadBy('name',' Color');
+				
+				$model_cf_asso = $this->add('xepan\commerce\Model_Item_CustomField_Association');
+				$model_cf_asso->addCondition('customfield_generic_id',$spec_m->id);
+				$model_cf_asso->addCondition('item_id',$item_m->id);
+				$model_cf_asso->tryLoadAny();
+
+				$model_cf_value = $this->add('xepan\commerce\Model_Item_CustomField_Value')
+									   ->addCondition('customfield_association_id', $model_cf_asso->id);
+			    $model_cf_value->tryLoadAny();
+
+				if($this->options['filter_type'] == 'shop'){
+					$stock_m = $this->add('xepan\commerce\Model_ItemStock');
+					$stock_m->addCondition('item_id',$item_m->id);
+					$stock_m->addCondition('category',$category_m->id);
+
+					if($stock_m['current_stock'] > 0)
+			    		$spec_array [] = $model_cf_value['name'];
+				}			    		
+			    else{
+			    	$spec_array [] = $model_cf_value['name'];
+			    }
+
+				$spec_m->unload();
+				$model_cf_asso->unload();
+				$model_cf_value->unload();
+
+				$custom_field = $this->add('xepan\commerce\Model_Item_CustomField');
+				$custom_field->loadBy('name','Size');
+
+				$model_cf_asso = $this->add('xepan\commerce\Model_Item_CustomField_Association');
+				$model_cf_asso->addCondition('customfield_generic_id',$custom_field->id);
+				$model_cf_asso->addCondition('item_id',$item_m->id);
+				$model_cf_asso->tryLoadAny();
+
+				$model_cf_value = $this->add('xepan\commerce\Model_Item_CustomField_Value');
+				$model_cf_value->addCondition('customfield_association_id',$model_cf_asso->id);
+
+				foreach ($model_cf_value as $val) {
+					if(($this->options['filter_type'] == 'Shop')){
+						if(in_array($val['name'], array_unique($avil_size_array)))
+							$spec_array[] = $val['name'];
+					}
+					// else{
+					// 	$spec_array [] = $val['name']; 	
+					// }						
+				}
+
+				$custom_field->unload();					   
+				$model_cf_asso->unload();	
+				$model_cf_value->unload();
+			}
+		}
+
+		if(!$model_filter->count()->getOne()){
+			$this->add('View_Error')->set('no filter found');
+			return;
+		}
+
+		//Filter Form
+		$form = $this->add('Form',null,null,['form/empty']);
+
+		//price slider
+		if($this->options['show_price_filter']){
+			$this->heading = $form->add('View',null,null,[$form_layout]);
+
+			$price = $this->heading->addField('xepan\commerce\RangeSlider','price');
+			$price->min = $this->options['min_price']?:0;
+			$price->max = $this->options['max_price']?:10;
+			$price->step = $this->options['step']?:1;
+			$price->left = $this->options['left_label']?:'min';
+			$price->right = $this->options['right_label']?:'max';
+
+			if($price_range = $this->app->recall('price_range')){
+				$range_array = explode(",", $price_range);
+				$price->selected_min = $range_array[0];
+				$price->selected_max = $range_array[1];
+				$price->set($price_range);
+				// $this->app->forget('price_range');
+			}
+			$this->heading->template->trySet('name','Price Range '.$price->selected_min." - ".$price->selected_max);			
+		}
+
+		$q = $model_filter->dsql();
+		/**
+		get all unique value
+		Filterable specification has many Association
+		Association has many values
+		*/
+		//join with association
+		$asso_join = $model_filter->Join('customfield_association.customfield_generic_id','id');
+
+		//association join with values
+		$value_join = $asso_join->join('customfield_value.customfield_association_id','id');
+		$value_join->addField('value_name','name');
+		$value_join->addField('value_id','id');
+
+		//group by with value name
+		$cf_name_group_element = $q->expr('[0]',[$model_filter->getElement('id')]);
+		//group by with specification name
+		$value_group_element = $q->expr('[0]',[$model_filter->getElement('value_name')]);
+		$model_filter->_dsql()->group($value_group_element);
+
+		$model_filter->addCondition('value_name','<>',"");
+		$model_filter->setOrder('value_name','asc');
+		$model_filter->setOrder('name','asc');
+		
+		if(!empty($spec_array))
+			$model_filter->addCondition('value_name',$spec_array);
+		
+		$unique_specification_array = [];
+		$count = 1;
+	
+		foreach ($model_filter as $specification) {
+
+			if(!isset($unique_specification_array[$specification['name']])){
+				$this->heading = $form->add('View',null,null,['view/tool/filter/formsection']);
+				$this->heading->template->trySet('name',$specification['name']);
+				// $this->heading->add('H2')->set($specification['name']);
+				$unique_specification_array[$specification['name']] = [];
+			}
+			$field = $this->heading->addField('checkbox',$specification['value_id'],$specification['value_name']);
+			
+			if(count($previous_selected_filter)){
+
+				// echo "<pre>";
+					// print_r($previous_selected_filter[$specification['id']]['values']);
+				if(isset($previous_selected_filter[$specification['id']]))
+					if(in_array($specification['value_name'],$previous_selected_filter[$specification['id']]))
+						$field->set(1);
+			}
+			// $count++;
+		}
+		// $form->on('click','input',$form->js()->submit());
+		$form->on('change','input',$form->js()->submit());
+
+
+		// $url = "product/".$url;
+		//specification_id_1:value1,value2|specification_id_2:value_1,value_2
+		if($form->isSubmitted()){						
+			$selected_options = [];				
+			$str = "";
+			$specification_array=[];
+			// $count = 1;
+
+			foreach ($model_filter as $specification) {
+				//if filter checked or not
+				if($form[$specification['value_id']]){
+						
+					if(!isset($specification_array[$specification['id']]))
+						$specification_array[$specification['id']] = [];
+
+					$specification_array[$specification['id']][] = $specification['value_name'];
+					// echo '<pre>';
+					// var_dump($specification_array);
+					// exit;
+					
+				}
+				// $count++;
+			}
+
+			$this->app->memorize('filter',json_encode($specification_array,true));
+			$this->app->memorize('price_range',$form['price']);
+			
+			if($this->options['filter_type'] == 'Product'){
+				$url = 'product/'.$_GET['category_code'];
+				$this->app->stickyForget('category_code');
+				$form->app->redirect($this->app->url($url));
+			}			
+			elseif($this->options['filter_type'] == 'Shop'){
+				$url = 'shop/'.$_GET['category_code'];
+				$this->app->stickyForget('category_code');
+				$this->app->stickyForget('parent_category_code');
+				$form->app->redirect($this->app->url($url));
+			}
+			else	{
+				$form->app->redirect($this->app->url());
+			}
+		}
+
+	}
+
+	function render(){
+		$this->js(true)->_css("jquery-ui");
+		parent::render();
+	}
+}
---
+++ lib/Tool/ItemList.php
@@ -0,0 +1,481 @@
+<?php
+
+namespace xepan\custom;
+
+class Tool_ItemList extends \xepan\cms\View_Tool{
+	public $options = [
+
+					'show_item'=>"all", /* all,new,mostviewed,featured*/
+					'layout'=>'grid',/* grid,list*/
+					/*show or hide options*/
+					'show_name'=>true,
+					'show_image'=>true,
+					'show_sku'=>true,
+					'show_sale_price'=>true,
+					'show_original_price'=>true,
+					'show_description'=>true, 
+					'show_tags'=>true,
+					'show_specification'=>true,
+					'show_qty_unit'=>true,
+					'show_qty_selection'=>true,
+					'show_stock_availability'=>false,
+					'show_is_enquiry_allow'=>false,
+					'show_paginator'=>true,
+					'show_personalizedbtn'=>true,
+					'show_addtocart'=>true,
+					'show_multi_step_form'=>false,
+					'show_price_or_amount'=>false,
+					'filter-effect'=>false,
+					'show_item_count'=>true,
+					'show_category_name'=>false,
+					/**/
+					'personalized_page_url'=>'',
+					'personalized_button_name'=>'Personalize',
+					'paginator_set_rows_per_page'=>"4",
+					'show_shipping_charge'=>true,
+					'shipping_charge_with_item_amount'=>true,
+					'show_item_of_category'=>"",
+					'custom_template'=>'',
+					'show_microdata'=>true,
+					'amount_group_in_multistepform'=>null,
+					'show_out_of_stock'=>true,
+					'hide_items'=>''
+				];
+
+	public $complete_lister=null;
+
+	function init(){
+		parent::init();		
+		
+		//Validate Required Options Value
+		$message = $this->validateRequiredOptions();
+		if($message != 1){
+			$this->add('View_Warning')->set($message);
+			return;
+		}
+		if($this->options['show_microdata']){
+			$this->company_m = $this->add('xepan\base\Model_ConfigJsonModel',
+						[
+							'fields'=>[
+										'company_name'=>"Line",
+										'company_owner'=>"Line",
+										'mobile_no'=>"Line",
+										'company_email'=>"Line",
+										'company_address'=>"Line",
+										'company_pin_code'=>"Line",
+										'company_description'=>"text",
+										],
+							'config_key'=>'COMPANY_AND_OWNER_INFORMATION',
+							'application'=>'communication'
+						]);
+			$this->company_m->tryLoadAny();
+		}
+
+		$item = $this->add('xepan\commerce\Model_Item_WebsiteDisplay');
+		$item->addCondition('status','Published');
+
+		$q = $item->dsql();
+		
+		$this->app->stickyGET('parent_category_id');
+		$this->app->stickyGET('xsnb_category_id');
+		/**
+		category wise filter
+		*/
+		//tool options show only category item
+		$selected_category = [];
+		if($this->options["show_item_of_category"]){
+			$selected_category = explode(",", $this->options["show_item_of_category"]);
+		}elseif($_GET['xsnb_category_id'] and is_numeric($_GET['xsnb_category_id'])){
+			$selected_category[] = $_GET['xsnb_category_id'];
+		}
+
+		if($category_code = $_GET['category_code']){
+			$category_code = explode('/', $category_code);
+			$cat_m = $this->add('xepan\commerce\Model_Category');
+			$cat_m->tryLoadBy('slug_url',$category_code[1]);			
+			$selected_category[] = $cat_m->id;
+		}
+
+		if(count($selected_category)){
+			$item_join = $item->Join('category_item_association.item_id');
+			$item_join->addField('category_id');
+			$item_join->addField('category_assos_item_id','item_id');
+			
+			$cat_join = $item_join->leftJoin('category.document_id','category_id');
+			$cat_join->addField('category_document_id','document_id');
+
+			$document_join = $cat_join->leftJoin('document.id','document_id');
+			$document_join->addField('category_status','status');
+
+			$item->addCondition('category_status',"Active");
+			$item->addCondition('category_id',$selected_category);
+			
+			$group_element = $q->expr('[0]',[$item->getElement('category_assos_item_id')]);
+		}
+
+		if($this->options['show_related_items']){
+			$current_item_id = $this->app->stickyGET('commerce_item_id');
+			$current_item_m = $this->add('xepan\commerce\Model_Item');
+			$current_item_m->addCondition('id',$current_item_id);
+
+			$item_j = $current_item_m->Join('category_item_association.item_id');
+			$item_j->addField('category_id');
+
+			$item_join = $item->Join('category_item_association.item_id');
+			$item_join->addField('category_id');
+			$item_join->addField('category_assos_item_id','item_id');
+			
+			$cat_join = $item_join->leftJoin('category.document_id','category_id');
+			$cat_join->addField('category_document_id','document_id');
+
+			$document_join = $cat_join->leftJoin('document.id','document_id');
+			$document_join->addField('category_status','status');
+
+			$item->addCondition('category_status',"Active");
+			$item->addCondition('category_id',$current_item_m['category_id']);
+			
+			$group_element = $q->expr('[0]',[$item->getElement('category_assos_item_id')]);
+		}
+
+		if(!$this->options['show_out_of_stock']){
+			$parent_category_id = $_GET['parent_category_id'];
+			if(!$parent_category_id){
+				$c = $this->add('xepan\commerce\Model_Category');
+				$c->loadBy('slug_url',$_GET['parent_category_code']);
+				$parent_category_id = $c->id;
+			}	
+
+			$item->addExpression('net_stock')->set(function($m,$q) use($parent_category_id){
+				$item_stock_m = $this->add('xepan\commerce\Model_ItemStock');
+				$item_stock_m->addCondition('item_id',$m->getElement('id'));
+				$item_stock_m->addCondition('category',$parent_category_id);
+
+				return $q->expr('IFNULL([0],"0")',[$item_stock_m->sum('current_stock')]);
+			});
+
+			$item->addCondition('net_stock','>',0);
+		}
+		
+		if($this->options['hide_items']){
+			if($this->options['hide_items'] === 'hide_in_shop'){
+				$item->addCondition($item->dsql()->orExpr()
+	    					     		->where('hide_in_shop',0)
+	    					     		->where('hide_in_shop',null)
+				       			);
+			}
+
+			if($this->options['hide_items'] === 'hide_in_product'){
+				$item->addCondition($item->dsql()->orExpr()
+	    					     		->where('hide_in_product',0)
+	    					     		->where('hide_in_product',null)
+				       			);
+			}
+		}
+
+		if($_GET['search']){
+			// $item->addExpression('Relevance')->set('MATCH(search_string) AGAINST ("'.$_GET['search'].'" IN NATURAL LANGUAGE MODE)');
+			$item->addExpression('Relevance')->set(function($m, $q){
+				return $q->expr('MATCH([0]) AGAINST ("[1]" IN NATURAL LANGUAGE MODE)',[$q->getField('search_string'),$_GET['search']]);
+			});
+			$item->addCondition('Relevance','>',0);
+	 		$item->setOrder('Relevance','Desc');
+		}
+
+		// //Price Range Search
+		if($price_range = $this->app->recall('price_range')){
+			$price_array = explode(",", $price_range);
+			$item->addCondition('sale_price','>=',$price_array[0]);
+			$item->addCondition('sale_price','<=',$price_array[1]);
+			$this->app->forget('price_range');
+		}
+
+		
+		// //Filter Search
+		$filter = $this->app->recall('filter',false);
+		$selected_filter_data_array = json_decode($filter,true);
+		if($this->options['filter-effect'] and count($selected_filter_data_array)){
+
+			$item_custom_field_asso_j = $item->Join('customfield_association.item_id','id');
+			$item_custom_field_asso_j->addField('customfield_generic_id');
+			$item_custom_field_asso_j->addField('specification_item_id','item_id');
+
+			$custom_field_j = $item_custom_field_asso_j->join('customfield_generic.id','customfield_generic_id');
+			$custom_field_j->addField('cf_is_filterable','is_filterable');
+
+			$item->addCondition('cf_is_filterable',true);
+
+			$cf_asso_value_j = $item_custom_field_asso_j->join('customfield_value.customfield_association_id','id');
+			$cf_asso_value_j->addField('value_name','name');
+			$cf_asso_value_j->addField('value_status','status');
+
+			$item->addCondition('value_status','Active');
+			
+			$cond=[];
+			foreach ($selected_filter_data_array as $specification_id => $values_array) {
+				if(empty($values_array)) continue;
+				
+				$or = $q->orExpr();
+				foreach ($values_array as $value) {
+					$value = str_replace("\"","",$value);
+					$or->where($q->expr('[0] = "[1]"',[$item->getElement('value_name'),$value]));
+				}
+
+				$item->addCondition($q->andExpr()
+									->where('customfield_generic_id',$specification_id)
+									->where($or)
+									);
+			}
+
+
+			$group_element = $q->expr('[0]',[$item->getElement('specification_item_id')]);
+			$item->_dsql()->group($group_element); // Multiple category association shows multiple times item so .. grouped
+
+			$this->app->forget('filter');
+		}
+
+
+		//load record according to sequence of order 
+		$item->setOrder('display_sequence','desc');
+		//$item->setOrder('name','asc');
+
+		$layout_template = $this->options['layout'];
+		
+		if($this->options['custom_template']){
+			$path = getcwd()."/websites/".$this->app->current_website_name."/www/view/tool/item/".$this->options['custom_template'].".html";
+			if(!file_exists($path)){
+				throw new \Exception($path);
+				$this->add('View_Warning')->set('template not found');
+				return;	
+			}else{
+				$layout_template = $this->options['custom_template'];
+				
+			}
+		}	
+		$this->complete_lister = $cl = $this->add('CompleteLister',null,null,['view/tool/item/'.$layout_template]);
+		//not record found
+		if(!$item->count()->getOne())
+			$cl->template->set('not_found_message','No Record Found');
+		else
+			$cl->template->del('not_found');
+
+		$designer = $this->add('xepan\base\Model_Contact');
+		if($designer_id = $this->app->stickyGET('designer_id')){
+			$item->addCondition('designer_id',$designer_id);
+			$designer->load($designer_id);	
+		}
+
+		$cl->setModel($item);
+
+		if( !($this->options['show_item_count'] or $this->options['show_category_name'])){
+			$cl->template->tryDel('item_count_wrapper');
+		}else{		
+			// show item count
+			if($this->options['show_item_count']){
+				$cl->template->trySet('item_count',$item->count()->getOne());
+			}
+			
+			if($this->options['show_category_name']){
+				$str = "";
+				foreach ($selected_category as $cat_id) {
+					$ct_model = $this->add('xepan\commerce\Model_Category')->tryLoad($cat_id);
+					if($ct_model->loaded()){
+						$str .= $ct_model['name'] .", ";
+					}
+				}
+				$cl->template->trySet('category_name',rtrim($str,", "));
+				if($designer_id)
+					$cl->template->trySet('designer_name',$designer['name']);
+			}
+		}	
+
+		if($this->options['show_paginator']=="true"){
+			$paginator = $cl->add('Paginator',['ipp'=>$this->options['paginator_set_rows_per_page']]);
+			$paginator->setRowsPerPage($this->options['paginator_set_rows_per_page']);
+		}
+
+
+		$cl->add('xepan\cms\Controller_Tool_Optionhelper',['options'=>$this->options,'model'=>$item]);
+
+		$self = $this;
+		$url = $this->app->url($this->options['personalized_page_url']);
+		//click in personilize btn redirect to personilize pag
+		$cl->on('click','.xepan-commerce-item-personalize',function($js,$data)use($url,$self){
+			$url = $self->app->url($url,['xsnb_design_item_id'=>$data['xsnbitemid']]);
+			return $js->univ()->location($url);
+		});
+
+	}
+
+	function render(){
+
+		$this->js(true)
+				->_load($this->api->url()->absolute()->getBaseURL().'vendor/xepan/commerce/templates/js/tool/jquery-elevatezoom.js')
+				->_load($this->api->url()->absolute()->getBaseURL().'vendor/xepan/commerce/templates/js/tool/jquery.fancybox.js');
+		
+		parent::render();
+	}
+
+	function addToolCondition_show_item($value,$model){
+
+		switch ($value) {
+			case 'all':
+				// No Need to any condition because on all it' show all item 
+				// $model->addCondition(
+				// 			$model->dsql()->orExpr()
+				// 				->where('is_new',true)
+				// 				->where('is_mostviewed',true)
+				// 				->where('is_feature',true)
+				// 			);
+				break;
+			case 'new':
+				$model->addCondition('is_new',true);
+				break;
+			case 'mostviewed':
+				$model->addCondition('is_mostviewed',true);
+				break;
+			case 'featured':
+				$model->addCondition('is_feature',true);
+				break;
+		}
+	}
+
+	function addToolCondition_row_show_personalizedbtn($value,$l){		
+		if($l->model['is_designable']){			
+			$btn = $l->add('Button',null,'personalizedbtn')
+				->addClass('xepan-commerce-item-personalize btn btn-primary btn-block')
+				->setAttr('data-xsnbitemid',$l->model->id)
+				;
+			$btn->set($this->options['personalized_button_name']?:'Personalize');
+			$l->current_row_html['personalizedbtn'] = $btn->getHtml();
+		}else{
+			// $l->current_row_html['personalizedbtn_wrapper'] = "";
+			$l->current_row_html['personalizedbtn'] = "";
+		 }
+	}
+
+	function addToolCondition_row_show_microdata($value,$l){
+		
+		$v=$this->add('CompleteLister',null,null,['view/schema-micro-data','Product_list_block']);
+		$v->setModel(clone $l->model);
+		
+		$v->addHook('formatRow',function($m){
+			// $m->current_row_html['company_name']=$this->company_m['company_name'];
+			$m->current_row_html['item_image']=$this->app->pm->base_url.$m->model['first_image'];
+			$m->current_row_html['currency']=$this->app->epan->default_currency->get('name');
+		});
+		$l->current_row_html['micro_data']=$v->getHtml();
+	}
+
+	function addToolCondition_row_show_image($value, $l){
+		if(!$value){
+			$l->current_row_html['image_wrapper'] = "";
+			return;
+		}
+
+		if(!$l->model['first_image'])
+			$l->current_row['first_image'] = "vendor/xepan/commerce/templates/view/tool/item/images/xepan_item_list_no_image.jpg";
+		
+	}
+
+	function addToolCondition_row_show_addtocart($value,$l){
+		
+		if($value != true){
+			$l->current_row_html['addtocart_wrapper'] = "";
+			return;
+		}
+
+		if($l->model['is_saleable']){
+			$options = [
+						'show_addtocart_button'=>'true',
+						'button_name'=>$this->options['addtocart_name'],
+						'show_shipping_charge'=>$this->options['show_shipping_charge'],
+						'shipping_charge_with_item_amount'=>$this->options['shipping_charge_with_item_amount'],
+						'show_price'=>$this->options['show_price_or_amount'],
+						'show_multi_step_form'=>$this->options['show_multi_step_form'],
+						'amount_group_in_multistepform'=>$this->options['amount_group_in_multistepform']
+						];
+
+			$cart_btn = $l->add('xepan\commerce\Tool_Item_AddToCartButton',
+					[
+						'name' => "addtocart_view_".$l->model->id,
+						'options'=>$options
+					],'Addtocart'
+				);
+		
+			$item = $this->add('xepan\commerce\Model_Item')->load($l->model->id);
+			$cart_btn->setModel($item);
+			$l->current_row_html['Addtocart'] = $cart_btn->getHtml();
+		}else
+			$l->current_row_html['Addtocart'] = "";
+
+	}
+
+	function addToolCondition_row_item_detail_page_url($value,$l){
+		$url = $this->api->url();
+		$detail_page_url = $this->api->url($this->options['item_detail_page_url'],['commerce_item_id'=>$l->model->id]);
+
+		if($this->options['name_redirect_to_detail'] == "true"){
+			$l->current_row_html['item_detail_page_url_via_name'] = $detail_page_url;
+		}else{			
+			$l->current_row_html['item_detail_page_url_via_name'] = $url;
+		}
+
+		if($this->options['image_redirect_to_detail'] == "true")
+			$l->current_row_html['item_detail_page_url_via_image'] = $detail_page_url;
+		else
+			$l->current_row_html['item_detail_page_url_via_image'] = $url;
+			
+	}
+
+	function addToolCondition_row_show_specification($value,$l){
+		
+		if(!$value){
+			$l->current_row_html['specification']='';
+			return;
+		}
+
+		$l->model->specifications = $specification = $l->model->specification(null,$highlight_only = true);
+		$temp = $l->add('CompleteLister',null,'specification',['view/tool/item/'.$this->options['layout'],'specification']);
+		$temp->setModel($specification);
+
+		$l->current_row_html['specification'] = $temp->getHtml();
+	}
+
+	function addToolCondition_row_show_description($value,$l){
+		if(!$value){
+			$l->current_row_html['description']='';
+			return;
+		}
+		if($this->options['show_description']){
+			$l->current_row_html['description']=$l->model['description'];
+		}else{
+			$l->current_row_html['description']=" ";
+		}
+	}
+
+	function addToolCondition_row_show_shipping_charge($value,$l){
+		if(!$value){
+			$l->current_row_html['shipping_charge'] = "";
+			return;
+		}
+
+		if($this->options['shipping_charge_with_item_amount']){
+			$l->current_row_html['shipping_charge'] = "";
+			$l->current_row_html['shipping_charge_wrapper'] = "";
+		}else
+			$l->current_row_html['shipping_charge'] = "0";	
+
+	}
+
+	function validateRequiredOptions(){
+		return true;
+	}
+
+	function getTemplate(){
+		return $this->complete_lister->template;
+	}
+
+	function getTemplateFile(){
+		return $this->complete_lister->template->origin_filename;
+	}
+}
